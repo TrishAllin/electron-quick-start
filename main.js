70625a26e62d7ebcf342b64d31facc8fd0651a29
Trisha Allin <tallin@nio-mbpro-tallin.local>
2016-10-27 11:44:25 -0600

    James does magic

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 1200, height: 600})
 
   // and load the index.html of the app.
-  mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+  mainWindow.loadURL(`file://${__dirname}/public/index.html`)
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools()
